a9ac37ee5a7da74604d5bb322344c6406f05e0c8
cym2050 <1390062269@qq.com>
2020-03-27 23:23:44 +0800

    实现Cat.Weight的策略

--- src/com/cym2050/strategy/Main.java
+++ src/com/cym2050/strategy/Main.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //int[] a = {9, 2, 3, 5, 7, 1, 4};
-        //Cat[] cat = {new Cat(1, 1), new Cat(3,3), new Cat(2, 2)};
-        Dog[] dogs = {new Dog(5), new Dog(6), new Dog(2)};
+        Cat[] cats = {new Cat(1, 1), new Cat(3,3), new Cat(2, 2)};
+        //Dog[] dogs = {new Dog(5), new Dog(6), new Dog(2)};
         Sorter sorter = new Sorter();
-        sorter.sort(dogs, new DogComparator());
-        System.out.println(Arrays.toString(dogs));
+        //sorter.sort(dogs, new DogComparator());
+        sorter.sort(cats, new CatWeightComparator());
+        System.out.println(Arrays.toString(cats));
     }
 }
