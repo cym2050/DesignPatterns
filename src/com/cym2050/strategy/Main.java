89d637eb6c3755969b9181eaeabeb90d2121e44d
cym2050 <1390062269@qq.com>
2020-03-27 23:27:31 +0800

    猫身高比较用策略实现

--- src/com/cym2050/strategy/Main.java
+++ src/com/cym2050/strategy/Main.java
@@ -8,8 +8,9 @@ public class Main {
         Cat[] cats = {new Cat(1, 1), new Cat(3,3), new Cat(2, 2)};
         //Dog[] dogs = {new Dog(5), new Dog(6), new Dog(2)};
         Sorter sorter = new Sorter();
-        //sorter.sort(dogs, new DogComparator());
-        sorter.sort(cats, new CatWeightComparator());
+        //sorter.sort(dogs, new DogComparator());       //狗策略
+        //sorter.sort(cats, new CatWeightComparator()); //猫体重策略
+        sorter.sort(cats, new CatHeightComparator());   //猫身高策略
         System.out.println(Arrays.toString(cats));
     }
 }
